2355d23a35ff27da58162f18098df00118ea69f5
Alexandre Akira <alexandreakiraakao@gmail.com>
2020-08-04 02:05:55 -0300

    config global

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import logoImg from '../../assets/images/logo.svg';
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
