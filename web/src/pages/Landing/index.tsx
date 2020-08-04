d378db028fd670182cbb6b8bae8927bdddbfb5eb
Alexandre Akira <alexandreakiraakao@gmail.com>
2020-08-04 02:47:18 -0300

    ladingpage

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
 
 function Landing() {
   return (
@@ -8,7 +14,31 @@ function Landing() {
       <div id="page-landing-content" className="container">
         <div className="logo-container">
           <img src={logoImg} alt="Proffy" />
+          <h2>Sua plataforma de estudos online.</h2>
         </div>
+
+        <img 
+          src={landingImg} 
+          alt="Plataforma de estudos" 
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <a href="" target="_blank" className="study">
+            <img src={studyIcon} alt="Estudar"/>
+            Estudar
+          </a>
+
+          <a href="" target="_blank" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas"/>
+            Dar aulas
+          </a>
+        </div>
+
+        <span className="total-connections">
+          Total de 200 conexões já realizadas 
+          <img src={purpleHeartIcon} alt="Coração roxo"/>
+        </span>
       </div>
     </div>
   );
