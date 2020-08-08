18768126d10d52cbdddc306e99a066118c459e0b
Alexandre Akira <alexandreakiraakao@gmail.com>
2020-08-08 09:15:23 -0300

    teacherList connect with back

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,21 +3,41 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://i.pravatar.cc/300" alt="random"/>
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Name</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Mussum Ipsum, cacilds vidis litro abertis. 
-        Tá deprimidis, eu conheço uma cachacis que pode alegrar sua vidis.
+        {teacher.bio}
         <br /><br />
         Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, 
         vitae iaculis nisl. Mé faiz elementum girarzis, nisi eros vermeio. 
@@ -27,12 +47,17 @@ function TeacherItem() {
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          href={`https://wa.me/${teacher.whatsapp}`} 
+          target="_blank" 
+          rel="noopener noreferrer"
+          onClick={createNewConnection}
+        >
           <img src={whatsappIcon} alt="whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
