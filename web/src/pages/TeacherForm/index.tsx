398ce7892aa71c807659a216eec0dcec20c30fb5
Alexandre Akira <alexandreakiraakao@gmail.com>
2020-08-08 07:18:07 -0300

    componente de input e estilização do teachForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
 
 function TeacherForm() {
   return (
-    <div id="page-teacher-list" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas." />
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer dar aulas." 
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus Dados</legend>
+
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Custo da sua hora por aula" />
+          
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
+      </main>
     </div>
   );
 }
