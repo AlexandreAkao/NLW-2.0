2cd3ab901ab21a7f6e33ac1b83d4dc39b5cf3b72
alexandreakira <alexandreakiraakao@gmail.com>
2020-11-11 01:04:29 -0300

    fix mobile folder

---
+++ mobile/App.tsx
@@ -0,0 +1,28 @@
+import { AppLoading } from 'expo';
+import { StatusBar } from 'expo-status-bar';
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+import React from 'react';
+
+import Landing from './src/pages/Landing';
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style="light" />
+      </>
+    );
+  }
+
+}
