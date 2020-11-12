4b1aa654cc79110a7190724deff406d8799ce8ec
alexandreakira <alexandreakiraakao@gmail.com>
2020-11-12 12:06:54 -0300

    router tabs

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './styles';
+
+export default function Favorites() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
